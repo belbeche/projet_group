9f116156950278c96454d7f7b03090b45ce5e0df
Kayem <abdoulatuf.pro@gmail.com>
2023-03-29 14:17:52 +0200

    intégration de la page de présentation des livres

---
+++ vues/librairie.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livraria</title>
+    <link rel="stylesheet" href="assets/css/librairie.css">
+</head>
+<body>
+<input type="checkbox" id="check" />
+      <header>
+        <h2><a href="#" class="logo">Livraria</a></h2>
+        <nav class="navigation">
+          <a href="#">Acceuil</a>
+          <a href="#">Connexion</a>
+          <a href="#">Inscription</a>
+        </nav>
+        <label for="check">
+          <i class="fas fa-bars menu-btn"></i>
+          <i class="fas fa-times close-btn"></i>
+        </label>
+      </header>
+
+      <h1 style="text-align: center;">NOS LIVRES</h1>
+    <div class="bigContainer">
+        <div class="container">
+          <div class="card">
+            <div class="imgBX">
+              <img
+                src="https://images.unsplash.com/photo-1592496431122-2349e0fbc666?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9vayUyMGNvdmVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                alt="image"
+              />
+            </div>
+            <div class="content">
+              <h2>Livre 1</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde est
+                impedit delectus totam sapiente deleniti, cum aperiam nesciunt error
+                ducimus architecto aut nostrum dolorem sed doloremque maxime
+                incidunt qui alias!
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="imgBX">
+              <img
+                src="https://images.unsplash.com/photo-1589829085413-56de8ae18c73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Ym9vayUyMGNvdmVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+                alt="image2"
+              />
+            </div>
+            <div class="content">
+              <h2>Livre 2</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde est
+                impedit delectus totam sapiente deleniti, cum aperiam nesciunt error
+                ducimus architecto aut nostrum dolorem sed doloremque maxime
+                incidunt qui alias!
+              </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="imgBX">
+              <img
+                src="https://images.unsplash.com/photo-1485125457414-5f3aa5b0d208?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGJvb2slMjBjb3ZlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                alt="image 3"
+              />
+            </div>
+            <div class="content">
+              <h2>Livre 3</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde est
+                impedit delectus totam sapiente deleniti, cum aperiam nesciunt error
+                ducimus architecto aut nostrum dolorem sed doloremque maxime
+                incidunt qui alias!
+              </p>
+            </div>
+          </div>
+        </div>
+
+    </div>
+</body>
+</html>
